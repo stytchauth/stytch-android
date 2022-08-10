d96f6af69d44ecc9b3bfbf559a07813cb2145455
Demid <ilkevic.demid@gmail.com>
2022-08-10 15:09:18 +0300

    remove ui

--- testapp/src/main/java/com/stytch/testapp/MainActivity.kt
+++ testapp/src/main/java/com/stytch/testapp/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.stytch.testapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
-import com.stytch.sdk.Stytch
-import com.stytch.sdk.StytchApi
-import com.stytch.sdk.StytchEnvironment
-import com.stytch.sdk.StytchUI
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -21,11 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Stytch.configure(
-            publicToken = "public-token-test-6906b71d-b14d-403c-becc-c4c88ac5fcfa",
-            environment = StytchEnvironment.TEST,
-        )
 
         findViewById<Button>(R.id.toggle_activity_button).apply {
             text = "Switch to Java Activity"
@@ -52,7 +42,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun switchToJavaActivity() {
-        startActivity(Intent(this, JavaActivity::class.java))
     }
 
     private fun toggleDarkMode() {
@@ -72,39 +61,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun testMagicLinkDirectApi() {
-        GlobalScope.launch {
-            val result = StytchApi.MagicLinks.Email.loginOrCreate(
-                email = "kyle@stytch.com",
-                loginMagicLinkUrl = "https://test.stytch.com/login",
-                signupMagicLinkUrl = "https://test.stytch.com/signup",
-            )
-            showResult(result)
-        }
+
     }
 
     private fun testEmailMagicLinkUIFlow() {
-        StytchUI.EmailMagicLink.configure(
-            loginMagicLinkUrl = "https://test.stytch.com/login",
-            signupMagicLinkUrl = "https://test.stytch.com/signup",
-            createUserAsPending = true,
-            authenticator = { token ->
-                showResult("Received token '$token'")
-                StytchUI.onTokenAuthenticated()
-            }
-        )
-        val intent = StytchUI.EmailMagicLink.createIntent(this)
-        startActivity(intent)
     }
 
     private fun testSMSPasscodeUIFlow() {
-        StytchUI.SMSPasscode.configure(
-            createUserAsPending = true,
-            authenticator = { methodId, token ->
-                showResult("Received methodId '$methodId' and token '$token'")
-                StytchUI.onTokenAuthenticated()
-            }
-        )
-        val intent = StytchUI.SMSPasscode.createIntent(this)
-        startActivity(intent)
+
     }
 }
