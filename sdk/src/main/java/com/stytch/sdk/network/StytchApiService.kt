7fd588c0478c51b3077b995185542f8de2aab7f7
Tomas Michalkevic <tomas.michalkevic@gmail.com>
2022-08-15 13:40:48 +0300

    Initial code for OTP requests

--- sdk/src/main/java/com/stytch/sdk/network/StytchApiService.kt
+++ sdk/src/main/java/com/stytch/sdk/network/StytchApiService.kt
@@ -12,6 +12,26 @@ internal interface StytchApiService {
 
     @POST("magic_links/authenticate")
     suspend fun authenticate(
-        @Body request: StytchRequests.MagicLinks.Authenticate
+        @Body request: StytchRequests.MagicLinks.Authenticate,
+    ): StytchResponses.BasicResponse
+
+    @POST("otps/sms/login_or_create")
+    suspend fun loginOrCreateUserBySMS(
+        @Body request: StytchRequests.OTP.SMS,
+    ): StytchResponses.BasicResponse
+
+    @POST("otps/whatsapp/login_or_create")
+    suspend fun loginOrCreateByWhatsapp(
+        @Body request: StytchRequests.OTP.Whatsapp,
+    ): StytchResponses.BasicResponse
+
+    @POST("otps/email/login_or_create")
+    suspend fun loginOrCreateByEmail(
+        @Body request: StytchRequests.OTP.Email,
+    ): StytchResponses.BasicResponse
+
+    @POST("otps/authenticate") // TODO Need to create a proper name to differentiate fom magiclinks authenticate
+    suspend fun authenticateWithOTP(
+        @Body request: StytchRequests.OTP.Authenticate,
     ): StytchResponses.BasicResponse
 }
