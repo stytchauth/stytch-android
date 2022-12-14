88c868453ff1d0e8513478d9bf5f7fa4e3d20cd4
Jordan Haven <jhaven@stytch.com>
2022-12-14 14:54:10 -0500

    lint

--- sdk/src/main/java/com/stytch/sdk/network/StytchResponses.kt
+++ sdk/src/main/java/com/stytch/sdk/network/StytchResponses.kt
@@ -1,6 +1,5 @@
 package com.stytch.sdk.network
 
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.stytch.sdk.network.responseData.AuthData
 import com.stytch.sdk.network.responseData.BasicData
