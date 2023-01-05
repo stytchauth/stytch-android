47890a864bd5551f1be668166ada3579dfeee585
Jordan Haven <jhaven@stytch.com>
2023-01-05 10:46:54 -0500

    Extract uri builder

--- sdk/src/main/java/com/stytch/sdk/ThirdPartyOAuthImpl.kt
+++ sdk/src/main/java/com/stytch/sdk/ThirdPartyOAuthImpl.kt
@@ -8,28 +8,31 @@ internal class ThirdPartyOAuthImpl(
     private val storageHelper: StorageHelper,
     override val providerName: String,
 ) : OAuth.ThirdParty {
-    override fun start(parameters: OAuth.ThirdParty.StartParameters) {
-        val pkce = storageHelper.generateHashedCodeChallenge().second
-        val token = StytchApi.publicToken
-        val host = if (StytchApi.isTestToken) Constants.TEST_API_URL else Constants.LIVE_API_URL
-        val baseUrl = "${host}public/oauth/$providerName/start"
-        val potentialParameters = mapOf(
-            "login_redirect_url" to parameters.loginRedirectUrl,
-            "signup_redirect_url" to parameters.signupRedirectUrl,
-            "custom_scopes" to parameters.customScopes?.joinToString(" ")
-        )
-        val requestUri = Uri.parse(baseUrl)
+    private fun buildUri(host: String, parameters: Map<String, String?>, pkce: String, token: String): Uri {
+        return Uri.parse("${host}public/oauth/$providerName/start")
             .buildUpon()
             .apply {
                 appendQueryParameter("code_challenge", pkce)
                 appendQueryParameter("public_token", token)
-                potentialParameters.forEach {
+                parameters.forEach {
                     if (it.value != null) {
                         appendQueryParameter(it.key, it.value)
                     }
                 }
             }
             .build()
+    }
+
+    override fun start(parameters: OAuth.ThirdParty.StartParameters) {
+        val pkce = storageHelper.generateHashedCodeChallenge().second
+        val token = StytchApi.publicToken
+        val host = if (StytchApi.isTestToken) Constants.TEST_API_URL else Constants.LIVE_API_URL
+        val potentialParameters = mapOf(
+            "login_redirect_url" to parameters.loginRedirectUrl,
+            "signup_redirect_url" to parameters.signupRedirectUrl,
+            "custom_scopes" to parameters.customScopes?.joinToString(" ")
+        )
+        val requestUri = buildUri(host, potentialParameters, pkce, token)
         val customTabsIntent = CustomTabsIntent.Builder().build()
         customTabsIntent.launchUrl(parameters.context, requestUri)
     }
