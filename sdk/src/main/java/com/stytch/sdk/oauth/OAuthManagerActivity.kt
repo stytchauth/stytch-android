67b6223bb85cea50ad0c6d6914ed1932d1bf6c48
Jordan Haven <jhaven@stytch.com>
2023-01-09 10:31:06 -0500

    Add browser selector logic and some documentation. TODO: Cancellation intents should pass reason

--- sdk/src/main/java/com/stytch/sdk/oauth/OAuthManagerActivity.kt
+++ sdk/src/main/java/com/stytch/sdk/oauth/OAuthManagerActivity.kt
@@ -12,9 +12,41 @@ public enum class OAuthError(public val message: String) {
     NO_BROWSER_FOUND("No supported browser was found on this device"),
     NO_URI_FOUND("No OAuth URI could be found in the bundle"),
     USER_CANCELED("The user canceled the OAuth flow"),
-    OAUTH_ERROR_STATE("The returned OAuth URI is invalid"),
 }
 
+/**
+ * State management activity for OAuth flow. This is based on the functionality of AppAuth-Android
+ *
+ * The following diagram illustrates the operation of the activity:
+ *
+ *                          Back Stack Towards Top
+ *                +------------------------------------------>
+ *
+ * +------------+            +---------------+      +----------------+      +--------------+
+ * |            |     (1)    |               | (2)  |                | (S1) |              |
+ * | Initiating +----------->| OAuthManager  +----->| Authorization  +----->| OAuthReceiver|
+ * |  Activity  |            |   Activity    |      |   Activity     |      |   Activity   |
+ * |            |<-----------+               |<-----+ (e.g. browser) |      |              |
+ * |            | (S2, C2)   |               | (C1) |                |      |              |
+ * +------------+            +---------------+      +----------------+      +-------+------+
+ *
+ * - Step 1: ThirdPartyOAuth intiates an intent which launches this (no-ui) activity
+ * - Step 2: This activity determines the best browser to launch the authorization flow in, and launches it. Depending
+ *   on user action, we then enter either a cancellation (C)  or success (S) flow
+ *
+ * Cancellation (C) flow:
+ * If the user cancels the authorization, we are returned to this Activity at the top of the backstack (C1). Since no
+ * return URI is provided, we know the user cancelled, and return a RESULT_CANCELED result for the original intent (C2)
+ * and finish the activity. The calling activity will listen for this result and either provide messaging for the user
+ * if the error returned is one of NO_BROWSER_FOUND or NO_URI_FOUND, or (most likely) do nothing if it is USER_CANCELED.
+ *
+ * Success (S) flow:
+ * When the user completes authorization, the OAuthReceiverActivity is launched (S1), as specified in the manifest. That
+ * activity will launch this activity via an intent with CLEAR_TOP set, so that the authorization activity and receiver
+ * activity are destroyed leaving this activity at the top of the backstack. This activity will then return a
+ * RESULT_OK status for the original intent and pass along the returned URI, then finish itself (S2). The calling
+ * activity will listen for this result and use the returned URI to make the authorization call to the Stytch API.
+ */
 public class OAuthManagerActivity : Activity() {
     private var authorizationStarted = false
     private lateinit var desiredUri: Uri
@@ -33,7 +65,8 @@ public class OAuthManagerActivity : Activity() {
         // on first run, launch the intent to start the OAuth flow in the browser
         if (!authorizationStarted) {
             try {
-                val authorizationIntent = generateIntentForUri(desiredUri)
+                val browser = BrowserSelector.getBestBrowser(this) ?: throw ActivityNotFoundException()
+                val authorizationIntent = generateIntentForUri(browser, desiredUri)
                 startActivity(authorizationIntent)
                 authorizationStarted = true
             } catch (_: UninitializedPropertyAccessException) {
@@ -68,12 +101,13 @@ public class OAuthManagerActivity : Activity() {
         }
     }
 
-    private fun generateIntentForUri(uri: Uri): Intent {
-        // if using custom tabs browser
-        val customTabsIntent = CustomTabsIntent.Builder().build()
-        customTabsIntent.intent.data = uri
-        return customTabsIntent.intent
-        // else
+    private fun generateIntentForUri(browser: Browser, uri: Uri): Intent = if (browser.supportsCustomTabs) {
+        CustomTabsIntent.Builder().build().intent
+    } else {
+        Intent(Intent.ACTION_VIEW)
+    }.apply {
+        setPackage(browser.packageName)
+        data = uri
     }
 
     private fun authorizationComplete(uri: Uri) {
