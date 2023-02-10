fc429b8bef10d383e9febd03ed03ead41ec8da34
Jordan Haven <jhaven@stytch.com>
2023-02-10 16:15:04 -0500

    Add comments to organization interface

--- sdk/src/main/java/com/stytch/sdk/b2b/organizations/Organizations.kt
+++ sdk/src/main/java/com/stytch/sdk/b2b/organizations/Organizations.kt
@@ -2,12 +2,29 @@ package com.stytch.sdk.b2b.organizations
 
 import com.stytch.sdk.b2b.OrganizationResponse
 
+/**
+ * Organizations interface that encompasses retrieving an Organization
+ */
 public interface Organizations {
+    /**
+     * Data class used for wrapping parameters used with getting an Organization
+     * @param organizationId is the member's organization ID
+     */
     public data class GetOrganizationParameters(
         val organizationId: String
     )
 
+    /**
+     * Wraps Stytch’s organization/:organizationId endpoint.
+     * @param parameters required to receive magic link
+     * @return OrganizationResponse response from backend
+     */
     public suspend fun getOrganization(parameters: GetOrganizationParameters): OrganizationResponse
 
+    /**
+     * Wraps Stytch’s organization/:organizationId endpoint.
+     * @param parameters required to receive magic link
+     * @param callback calls callback with OrganizationResponse response from backend
+     */
     public fun getOrganization(parameters: GetOrganizationParameters, callback: (OrganizationResponse) -> Unit)
 }
