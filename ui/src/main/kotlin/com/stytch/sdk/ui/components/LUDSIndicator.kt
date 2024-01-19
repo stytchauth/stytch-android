b8fa9efc28b1689e2ee338896a09e6d1080c256a
Jordan Haven <jhaven@stytch.com>
2024-01-19 16:00:10 -0500

    Fix alignment of LUDS indicators

--- ui/src/main/kotlin/com/stytch/sdk/ui/components/LUDSIndicator.kt
+++ ui/src/main/kotlin/com/stytch/sdk/ui/components/LUDSIndicator.kt
@@ -3,8 +3,10 @@ package com.stytch.sdk.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,7 +15,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.stytch.sdk.b2b.network.models.StrengthCheckResponseData
 import com.stytch.sdk.consumer.network.models.LUDSRequirements
 import com.stytch.sdk.ui.R
 import com.stytch.sdk.ui.theme.LocalStytchBootstrapData
@@ -33,7 +37,7 @@ internal fun LUDSIndicator(
     Column(
         modifier = Modifier.padding(bottom = 16.dp)
     ) {
-        Row(verticalAlignment = Alignment.Top) {
+        Row {
             Image(
                 painter = if (validLength) {
                     painterResource(id = R.drawable.checkicon)
@@ -43,7 +47,7 @@ internal fun LUDSIndicator(
                 contentDescription = null,
                 modifier = Modifier
                     .width(16.dp)
-                    .padding(end = 4.dp)
+                    .padding(top = 6.dp, end = 4.dp)
             )
             Text(
                 text = stringResource(
@@ -69,7 +73,7 @@ internal fun LUDSIndicator(
                 contentDescription = null,
                 modifier = Modifier
                     .width(16.dp)
-                    .padding(end = 4.dp)
+                    .padding(top = 6.dp, end = 4.dp)
             )
             Text(
                 text = stringResource(
@@ -103,5 +107,24 @@ internal fun LUDSIndicator(
                 )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewLUDSIndicator() {
+    val requirements: LUDSRequirements = LUDSRequirements(
+        hasLowerCase = false,
+        hasUpperCase = false,
+        hasDigit = false,
+        hasSymbol = false,
+        missingCharacters = 0,
+        missingComplexity = 0
+    )
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(LocalStytchTheme.current.backgroundColor),
+    ) {
+        LUDSIndicator(requirements, false)
     }
 }
