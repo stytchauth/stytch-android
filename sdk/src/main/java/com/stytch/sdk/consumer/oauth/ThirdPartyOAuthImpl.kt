b07d09925ca3bac242e1a455dc537bb399b6a471
Jordan Haven <jhaven@stytch.com>
2024-04-15 16:42:39 -0400

    Update B2C OAuth and B2B SSO to support cname domains

--- sdk/src/main/java/com/stytch/sdk/consumer/oauth/ThirdPartyOAuthImpl.kt
+++ sdk/src/main/java/com/stytch/sdk/consumer/oauth/ThirdPartyOAuthImpl.kt
@@ -5,13 +5,19 @@ import com.stytch.sdk.common.Constants
 import com.stytch.sdk.common.StorageHelper
 import com.stytch.sdk.common.sso.SSOManagerActivity
 import com.stytch.sdk.common.sso.SSOManagerActivity.Companion.URI_KEY
+import com.stytch.sdk.consumer.StytchClient
 import com.stytch.sdk.consumer.network.StytchApi
 
 internal class ThirdPartyOAuthImpl(
     private val storageHelper: StorageHelper,
     override val providerName: String,
 ) : OAuth.ThirdParty {
-    internal fun buildUri(host: String, parameters: Map<String, String?>, pkce: String, token: String): Uri =
+    internal fun buildUri(
+        host: String,
+        parameters: Map<String, String?>,
+        pkce: String,
+        token: String,
+    ): Uri =
         Uri.parse("${host}public/oauth/$providerName/start")
             .buildUpon()
             .apply {
@@ -28,12 +34,16 @@ internal class ThirdPartyOAuthImpl(
     override fun start(parameters: OAuth.ThirdParty.StartParameters) {
         val pkce = storageHelper.generateHashedCodeChallenge().second
         val token = StytchApi.publicToken
-        val host = if (StytchApi.isTestToken) Constants.TEST_API_URL else Constants.LIVE_API_URL
-        val potentialParameters = mapOf(
-            "login_redirect_url" to parameters.loginRedirectUrl,
-            "signup_redirect_url" to parameters.signupRedirectUrl,
-            "custom_scopes" to parameters.customScopes?.joinToString(" ")
-        )
+        val host =
+            StytchClient.bootstrapData.cnameDomain?.let {
+                "https://$it/v1/"
+            } ?: if (StytchApi.isTestToken) Constants.TEST_API_URL else Constants.LIVE_API_URL
+        val potentialParameters =
+            mapOf(
+                "login_redirect_url" to parameters.loginRedirectUrl,
+                "signup_redirect_url" to parameters.signupRedirectUrl,
+                "custom_scopes" to parameters.customScopes?.joinToString(" "),
+            )
         val requestUri = buildUri(host, potentialParameters, pkce, token)
         val intent = SSOManagerActivity.createBaseIntent(parameters.context)
         intent.putExtra(URI_KEY, requestUri.toString())
