d5b2ba01dbba74dcfb0c82235e37aa7d61d15b0e
Jordan Haven <jhaven@stytch.com>
2024-08-21 14:38:32 -0400

    Add DFPPA domain for native DFP. NOTE: native DFP only accepts the domain, no-scheme

--- sdk/src/main/java/com/stytch/sdk/common/dfp/DFPProvider.kt
+++ sdk/src/main/java/com/stytch/sdk/common/dfp/DFPProvider.kt
@@ -12,12 +12,13 @@ internal interface DFPProvider {
 internal class DFPProviderImpl(
     context: Context,
     publicToken: String,
+    dfppaDomain: String,
 ) : DFPProvider {
     // We have to do this to allow unit tests to work, as they don't load native libs :( Alternatively, we'd need to
     // rewrite all of our affected tests to be instrumented tests, which isn't ideal
     private val dfp: NativeDFP? =
         try {
-            NativeDFP(context = context, publicToken = publicToken)
+            NativeDFP(context = context, publicToken = publicToken, submissionUrl = dfppaDomain)
         } catch (_: UnsatisfiedLinkError) {
             null
         } catch (_: NoClassDefFoundError) {
