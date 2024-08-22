0c570b4f5ea411d8c1951a990e9bb813f543c548
Jordan Haven <jhaven@stytch.com>
2024-08-22 11:46:18 -0400

    Update tests

--- sdk/src/test/java/com/stytch/sdk/consumer/StytchClientTest.kt
+++ sdk/src/test/java/com/stytch/sdk/consumer/StytchClientTest.kt
@@ -9,6 +9,7 @@ import com.stytch.sdk.common.DeeplinkResponse
 import com.stytch.sdk.common.DeviceInfo
 import com.stytch.sdk.common.EncryptionManager
 import com.stytch.sdk.common.StorageHelper
+import com.stytch.sdk.common.StytchClientOptions
 import com.stytch.sdk.common.StytchDispatchers
 import com.stytch.sdk.common.StytchResult
 import com.stytch.sdk.common.errors.StytchDeeplinkMissingTokenError
@@ -194,7 +195,7 @@ internal class StytchClientTest {
                 }
             coEvery { StytchApi.Sessions.authenticate(any()) } returns mockResponse
             val callback = spyk<(Boolean) -> Unit>()
-            StytchClient.configure(mContextMock, "", callback)
+            StytchClient.configure(mContextMock, "", StytchClientOptions(), callback)
             // callback is called with expected value
             verify(exactly = 1) { callback(true) }
             // isInitialized has fired
