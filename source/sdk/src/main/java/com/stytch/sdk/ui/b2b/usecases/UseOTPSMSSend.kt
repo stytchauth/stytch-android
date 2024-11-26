81ef72af4ae78d200d2904f298f1ee7d6521982e
Jordan Haven <jhaven@stytch.com>
2024-11-26 14:56:24 -0500

    Last of the functional hooks

--- source/sdk/src/main/java/com/stytch/sdk/ui/b2b/usecases/UseOTPSMSSend.kt
+++ source/sdk/src/main/java/com/stytch/sdk/ui/b2b/usecases/UseOTPSMSSend.kt
@@ -1,4 +1,37 @@
 package com.stytch.sdk.ui.b2b.usecases
 
-class UseOTPSMSSend {
+import com.stytch.sdk.b2b.StytchB2BClient
+import com.stytch.sdk.b2b.otp.OTP
+import com.stytch.sdk.common.network.models.BasicData
+import com.stytch.sdk.ui.b2b.Dispatch
+import com.stytch.sdk.ui.b2b.PerformRequest
+import com.stytch.sdk.ui.b2b.data.B2BUIState
+import com.stytch.sdk.ui.b2b.data.NavigateTo
+import com.stytch.sdk.ui.b2b.navigation.Routes
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+internal class UseOTPSMSSend(
+    private val scope: CoroutineScope,
+    private val state: StateFlow<B2BUIState>,
+    private val dispatch: Dispatch,
+    private val request: PerformRequest<BasicData>,
+) {
+    operator fun invoke() {
+        scope.launch(Dispatchers.IO) {
+            request {
+                StytchB2BClient.otp.sms.send(
+                    OTP.SMS.SendParameters(
+                        organizationId = state.value.mfaPrimaryInfoState?.organizationId ?: "",
+                        memberId = state.value.mfaPrimaryInfoState?.memberId ?: "",
+                        mfaPhoneNumber = state.value.phoneNumberState.toE164(),
+                    ),
+                )
+            }.onSuccess {
+                dispatch(NavigateTo(Routes.SMSOTPEntry))
+            }
+        }
+    }
 }
