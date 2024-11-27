ed6eeb67e1e9bc601eaa23d4a117dc99958e0d72
Jordan Haven <jhaven@stytch.com>
2024-11-27 10:13:35 -0500

    Make extension function for generating productcomponent ordering

---
+++ source/sdk/src/main/java/com/stytch/sdk/ui/b2b/data/ProductComponent.kt
@@ -0,0 +1,11 @@
+package com.stytch.sdk.ui.b2b.data
+
+internal enum class ProductComponent {
+    MagicLinkEmailForm,
+    MagicLinkEmailDiscoveryForm,
+    OAuthButtons,
+    SSOButtons,
+    PasswordsEmailForm,
+    PasswordEMLCombined,
+    Divider,
+}
