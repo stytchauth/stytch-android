3d99e91f79be9e526648cc5ec7f51755bddf0e54
Jordan Haven <jhaven@stytch.com>
2025-05-14 14:33:36 -0400

    Fix B2C UI memory leak

--- source/sdk/build.gradle.kts
+++ source/sdk/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 extra["PUBLISH_GROUP_ID"] = "com.stytch.sdk"
-extra["PUBLISH_VERSION"] = "0.47.3"
+extra["PUBLISH_VERSION"] = "0.47.4"
 extra["PUBLISH_ARTIFACT_ID"] = "sdk"
 
 apply("${rootProject.projectDir}/scripts/publish-module.gradle")
