cffcba2012fee5890036ee0e97830a1d3fe92f38
Jordan Haven <jhaven@stytch.com>
2025-07-07 14:57:49 -0400

    Fix jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,7 +68,7 @@ allprojects {
             force(rootProject.libs.guava)
             force(rootProject.libs.netty.codec.http)
             force("com.fasterxml.jackson.core:jackson-core:2.15.0")
-            force("com.fasterxml.jackson.core:jackson-databind:2.15.0.2")
+            force("com.fasterxml.jackson.core:jackson-databind:2.15.2")
         }
     }
 }
