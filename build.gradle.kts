12dd81a84a2c2aec463cc36e908dd4848f0c2ce0
Jordan Haven <jhaven@stytch.com>
2025-09-29 13:33:43 -0400

    Last one?

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,10 @@ buildscript {
         classpath(libs.versioning.plugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+        constraints {
+            // Force a newer version of commons-lang3 in transitive resolution
+            classpath("org.apache.commons:commons-lang3:3.18.0")
+        }
     }
 }
 
