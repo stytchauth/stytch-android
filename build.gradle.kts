fffbb1f192904bc5c8592f42c566dc53aac534d8
Jordan Haven <117691317+jhaven-stytch@users.noreply.github.com>
2025-10-16 12:07:53 -0400

    Fix publishing AGAIN (#386)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,16 @@ buildscript {
         classpath(libs.versioning.plugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+        configurations.all {
+            resolutionStrategy.eachDependency {
+                if (requested.group.startsWith("com.fasterxml.jackson")) {
+                    useVersion("2.19.1")
+                }
+                if (requested.name == "commons-compress") {
+                    useVersion("1.27.1")
+                }
+            }
+        }
     }
 }
 
